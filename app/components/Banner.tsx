fd271d267f1ae7eb0eda9cc417ae44d6b2627ea7
sherif altiby <sherifahmed852002@gmail.com>
2024-06-12 16:59:42 +0300

    add some changes

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -6,9 +6,6 @@ const Banner = () => {
         <Image src="https://images.unsplash.com/photo-1562832135-14a35d25edef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1245&q=80" alt="banner" fill className="object-cover object-left" />
         <div className="absolute top-1/2 w-full text-center" >
                 <p className="text-sm sm:text-lg text-gray-900 font-bold " >Not sure where to go? Perfect..</p>
-                <button className="text-purple-800 bg-white py-4 px-6 shadow:md rounded-full font-bold " >
-                        I am flexible
-                </button>
         </div>
     </div>
   )
